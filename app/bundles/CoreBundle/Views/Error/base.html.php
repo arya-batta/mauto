ce306672ca6e6efa7d46c171c6949fc2c7ec3f9a
kaviyarasans14 <kaviyarasans14@gmail.com>
2018-09-11 15:01:32 +0530

    Landing Page related GUI Changes

--- app/bundles/CoreBundle/Views/Error/base.html.php
+++ app/bundles/CoreBundle/Views/Error/base.html.php
@@ -23,22 +23,22 @@ $src = $view['mautibot']->getImage($img);
 
 $message = $view['slots']->get('message', 'mautic.core.error.generic');
 ?>
-<div class="pa-20 mautibot-error<?php if (!empty($inline)): echo ' inline well'; endif; ?>">
+<div class="pa-20 mautibot-error<?php if (!empty($inline)): echo ' inline well'; endif; ?>" style="position:relative;top:100px;">
     <div class="row mt-lg pa-md">
         <?php if ($isInline): ?>
             <div class="mautibot-content col-xs-12">
                 <h1><i class="fa fa-warning fa-fw text-danger"></i><?php echo $view['translator']->trans($message, ['%code%' => $status_code]); ?></h1>
-                <h4 class="mt-5"><strong><?php echo $status_code; ?></strong> <?php echo $status_text; ?></h4>
+                <h4 class="mt-5 hide"><strong><?php echo $status_code; ?></strong> <?php echo $status_text; ?></h4>
             </div>
         <?php else: ?>
 
-            <div class="mautibot-image col-xs-4 col-md-3">
+            <div class="mautibot-image col-xs-4 col-md-3" style="position:relative;bottom:45px;">
                 <img class="img-responsive" src="<?php echo $src; ?>" />
             </div>
             <div class="mautibot-content col-xs-8 col-md-9">
                 <blockquote class="np break-word">
                     <h1><i class="fa fa-quote-left"></i> <?php echo $view['translator']->trans($message, ['%code%' => $status_code]); ?> <i class="fa fa-quote-right"></i></h1>
-                    <h4 class="mt-5"><strong><?php echo $status_code; ?></strong> <?php echo $status_text; ?></h4>
+                    <h4 class="mt-5 hide"><strong><?php echo $status_code; ?></strong> <?php echo $status_text; ?></h4>
 
                     <footer class="text-right">LeadsEngageBot</footer>
                 </blockquote>
